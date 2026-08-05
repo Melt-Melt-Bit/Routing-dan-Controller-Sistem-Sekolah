3dd64980bbb4cf2e1de9f3bf8b06c5287227c348
Melt-Melt-Bit <MeltMelt3103@gmail.com>
2026-08-05 13:01:55 +0700

    feat: added major,teacher,classes on view and edited major and teacher controller

--- app/Http/Controllers/MajorController.php
+++ app/Http/Controllers/MajorController.php
@@ -8,12 +8,57 @@ class MajorController extends Controller
 {
     public function index()
     {
-        return "Menampilkan halaman daftar jurusan";
+        $title = "Sistem Sekolah - Daftar Jurusan";
+        $majors = [
+
+        [
+
+        'id' => 1,
+
+        'code' => 'AKL',
+
+        'name' => 'Akuntansi dan Keuangan Lembaga',
+
+        'description' => 'Program keahlian yang membekali murid dengan kompetensi pencatatan dan pelaporan keuangan.',
+
+        ],
+
+        [
+
+        'id' => 2,
+
+        'code' => 'TKJ',
+
+        'name' => 'Teknik Komputer dan Jaringan',
+
+        'description' => 'Program keahlian yang membekali murid dengan kompetensi instalasi, konfigurasi, dan pemeliharaan jaringan komputer.',
+
+        ],
+
+        [
+
+        'id' => 3,
+
+        'code' => 'BD',
+
+        'name' => 'Bisnis Digital',
+
+        'description' => 'Program keahlian yang membekali murid dengan kompetensi pemasaran dan pengelolaan bisnis berbasis digital.',
+
+        ],
+
+        ];
+        return view('majors.index', [
+            'title' => $title,
+            'majors' => $majors
+        ]);
     }
 
     public function create()
     {
-        return "Menampilkan halaman tambah jurusan";
+        return view('majors.create', [
+            'title' => 'Sistem Sekolah - Tambah Jurusan'
+        ]);
     }
 
     public function store(Request $request)
@@ -23,12 +68,16 @@ class MajorController extends Controller
 
     public function show(string $major)
     {
-        return "Menampilkan jurusan dengan ID: {$major}";
+        return view('majors.show', [
+            'title' => 'Sistem Sekolah - Detail Jurusan'
+        ]);
     }
 
     public function edit(string $major)
     {
-        return "Menampilkan halaman edit jurusan dengan ID: {$major}";
+        return view('majors.edit', [
+            'title' => 'Sistem Sekolah - Edit Jurusan'
+        ]);
     }
 
     public function update(Request $request, string $major)
--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -8,12 +8,57 @@ class TeacherController extends Controller
 {
     public function index()
     {
-        return "Menampilkan halaman daftar guru";
+        $title = "Sistem Sekolah - Daftar Guru";
+        $teachers = [
+
+        [
+
+        'id' => 1,
+
+        'nip' => '198501012024',
+
+        'name' => 'Budi Santoso',
+
+        'gender' => 'Laki-Laki',
+
+        'subject' => 'Akuntansi Dasar',
+
+        'phone' => '081234560001',
+
+        'status' => 'Aktif',
+
+        ],
+
+        [
+
+        'id' => 2,
+
+        'nip' => '198703152024',
+
+        'name' => 'Siti Aminah',
+
+        'gender' => 'Perempuan',
+
+        'subject' => 'Jaringan Komputer',
+
+        'phone' => '081234560002',
+
+        'status' => 'Aktif',
+
+        ]
+
+        ];
+        return view('teachers.index', [
+            'title' => $title,
+            'teachers' => $teachers
+        ]);
     }
 
     public function create()
     {
-        return "Menampilkan halaman tambah guru";
+        return view('teachers.create', [
+            'title' => 'Sistem Sekolah - Tambah Guru'
+        ]);
     }
 
     public function store(Request $request)
@@ -23,12 +68,16 @@ class TeacherController extends Controller
 
     public function show(string $teacher)
     {
-        return "Menampilkan guru dengan ID: {$teacher}";
+        return view('teachers.show', [
+            'title' => 'Sistem Sekolah - Detail Guru'
+        ]);
     }
 
     public function edit(string $teacher)
     {
-        return "Menampilkan halaman edit guru dengan ID: {$teacher}";
+        return view('teachers.edit', [
+            'title' => 'Sistem Sekolah - Edit Guru'
+        ]);
     }
 
     public function update(Request $request, string $teacher)
